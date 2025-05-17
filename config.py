0d448ec13fe44dd6aad355acb9db63d77c7eb435
Aeolian3 <130925153+Aeolian3@users.noreply.github.com>
2025-05-17 21:22:21 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import dotenv
 load_dotenv = dotenv.load_dotenv()
 
 class Config:
- TOKEN = dotenv.get_key(".env", "BOT_TOKEN")
+ TELEGRAM_BOT_TOKEN = dotenv.get_key(".env", "BOT_TOKEN")
  DB_URL = dotenv.get_key(".env", "DATABASE_URL")
