04721b3e42f114561020cff740553be25d6a8f68
Aeolian3 <130925153+Aeolian3@users.noreply.github.com>
2025-05-19 10:49:07 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-alembic upgrade head
+# Проверяем, существует ли папка alembic и файл конфигурации
+if [ -d "/app/alembic" ] && [ -f "/app/alembic.ini" ]; then
+    echo "Running Alembic migrations..."
+    alembic upgrade head
+else
+    echo "Alembic not configured. Skipping migrations."
+fi
+
+echo "Starting bot..."
 python -m main
